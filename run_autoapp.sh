4f99c37ceaf7f54c2920169267804db45184181d
Prashanth Santhirakumaran <contact@prasha.com.au>
2024-08-01 07:20:08 +0800

    Strip out old swc python scripts.

--- run_autoapp.sh
+++ run_autoapp.sh
@@ -3,20 +3,12 @@
 cd "${0%/*}"
 
 run_autoapp() {
-    until ./bin/autoapp; do
+    until ./bin/autoapp --platform eglfs; do
         echo "Crashed with exit code $?.  Respawning.." >&2
-    done
-}
-
-run_swc()  {
-    until python ./swc/swc.py; do
-        echo "Crashed with exit code $?.  Respawning.." >&2
-        sleep 1
     done
 }
 
 (trap 'kill 0' INT; \
-    run_swc > run_swc.log 2>&1 & \
     run_autoapp  > run_autoapp.log 2>&1 \
 )
 
